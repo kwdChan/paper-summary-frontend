a3b7201f487298c77297c2878a1f49e59cc5cfa8
dan <dan>
2023-04-28 16:19:25 +1000

    use link rather than a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 export default function IndexPage({}) {
@@ -5,13 +6,12 @@ export default function IndexPage({}) {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
-      <a
-        className="
-      inline-flex items-center m-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        href="/api/download"
+      <div className=" hover:cursor-pointer inline-flex items-center m-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <Link 
+                href="/api/download"
       >
         Download
-      </a>
+      </Link></div>
       <button
         className="
       inline-flex items-center m-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
