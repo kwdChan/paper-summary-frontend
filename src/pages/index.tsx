466660c7a529ac175183a9aec643511cf296910c
dan <dan>
2023-05-06 23:45:40 +1000

    using supabase storage for extension download

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+
+const DOWNLOAD_URL: string = process.env.NEXT_PUBLIC_DOWNLOAD_URL || "";
 export default function IndexPage({}) {
   const router = useRouter();
 
@@ -8,7 +10,7 @@ export default function IndexPage({}) {
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <div className=" hover:cursor-pointer inline-flex items-center m-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         <Link 
-                href="/api/download"
+                href={DOWNLOAD_URL}
       >
         Download
       </Link></div>
