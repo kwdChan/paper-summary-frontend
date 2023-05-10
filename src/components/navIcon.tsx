a2bfb40cbb706324fe0c7d58700b3b1a59994e88
dan <dan>
2023-05-10 22:09:53 +1000

    navIcon name changes

--- src/components/navIcon.tsx
+++ src/components/navIcon.tsx
@@ -64,14 +64,14 @@ export function NavIcon({}) {
           leaveTo="transform opacity-0 scale-95"
         >
           <Menu.Items className="absolute right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1"><MenuItem text='My Account' href='/dashboard' /></div>
+          <div className="px-1 py-1"><MenuItem text='My Usage' href='/dashboard' /></div>
             
             <div className="px-1 py-1 ">
 
             
 
-            <MenuItem text='All articles' href='/article' />
-            <MenuItem text='Delete mode' href='' onClick={deleteModeToggle} />
+            <MenuItem text='All Articles' href='/article' />
+            <MenuItem text='Delete Mode' href='' onClick={deleteModeToggle} />
             </div>
 
             <div className="px-1 py-1">
