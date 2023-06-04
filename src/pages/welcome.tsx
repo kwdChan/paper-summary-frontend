23cf1133f2ecda37554446e7bd7baa35a5a85769
dan <dan>
2023-06-04 19:16:02 +1000

    check meta tags from the extension for every 100ms

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,47 +1,70 @@
 import { supabaseClient } from "@/lib/supabaseClient";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import {ConfirmInChrome} from "@/components/authMatters/ConfirmInChrome";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { ConfirmInChrome } from "@/components/authMatters/ConfirmInChrome";
 
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
 
+function checkIfChrome(setIsOpen: Dispatch<SetStateAction<boolean>>) {
+  return setInterval(() => {
+    const extensionTag = document.querySelector(
+      'meta[name="review-express-extension"]'
+    );
+    if (extensionTag) {
+      setIsOpen(true);
+    }
+
+  }, 100);
+}
 
 export default function Page() {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
+  // useEffect(() => {
+  //   if (window.parent!=window) {
+  //     setIsOpen(true)
+  //   } else{
+  //     router.push("/article")
+  //   }
+
+  // }, [])
 
   useEffect(() => {
-    if (window.parent!=window) {
-      setIsOpen(true)
-    } else{
-      router.push("/article")
-    }
-
-  }, [])
+    // if it's in extension, there is a meta tag with name="review-express-extension"
+    const job = checkIfChrome(setIsOpen);
+    return () => clearInterval(job);
+  }, []);
 
   const submitCredentials = () => {
     supabaseClient.client.auth.getSession().then(({ data, error }) => {
-      window.parent.postMessage(
-        { message: 'signin', payload: { data, error }  }, 
-        '*'
-      );
-    })
-
-  }
+      window.postMessage({ message: "signin", payload: { data, error } }, "*");
+    });
+  };
 
   return (
     <div>
-      
       <div className="absolute top-1/2">
         Welcome! If you are from the web:{" "}
-        <Link href="/article" ><span className="text-blue-700 underline hover:cursor-pointer">click here</span></Link>
-        <br /><br />
+        <Link href="/article">
+          <span className="text-blue-700 underline hover:cursor-pointer">
+            click here
+          </span>
+        </Link>
+        <br />
+        <br />
         If you are from the extension, close the pop up and open it again.
       </div>
-      <ConfirmInChrome isOpen={isOpen} onAccept={submitCredentials} onCancel={()=>{supabaseClient.signout()}}/>
+      <ConfirmInChrome
+        isOpen={isOpen}
+        onAccept={submitCredentials}
+        onCancel={() => {
+          supabaseClient.signout();
+          router.push("/");
+        }}
+      />
     </div>
   );
 }
