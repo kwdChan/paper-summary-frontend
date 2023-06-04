5b5b76ded8a97b5925836294046044fa0d161a41
dan <dan>
2023-06-04 19:17:38 +1000

    a new page after extension login

---
+++ src/pages/howToUse.tsx
@@ -0,0 +1,14 @@
+import { supabaseClient } from "@/lib/supabaseClient";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { ConfirmInChrome } from "@/components/authMatters/ConfirmInChrome";
+
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+
+export default function Page() {
+  const router = useRouter();
+
+  return <>hello</>;
+}
