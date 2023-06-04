3c91cb80ec7dbc54247759f1cde7e8181d57e8cf
dan <dan>
2023-06-04 22:58:42 +1000

    warning styling

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -7,7 +7,7 @@ export function WarningBox({ title, children }:{title:string|null, children:stri
     return (
     <>
       {(up && (children || title)) ? (
-      <div className ='flex flex-row  absolute bottom-1 left-1 ring-1 rounded-sm  ring-yellow-600 bg-orange-100 text-yellow-700 '>
+      <div className ='flex flex-row  absolute bottom-1 left-1 mr-1 ring-1 rounded-sm  ring-yellow-600 bg-orange-100 text-yellow-700 '>
         <div className="flex flex-row items-center p-2">
           <ExclamationCircleIcon className="h-5 w-5 min-w-fit mr-2 " />
           <div>
@@ -16,7 +16,7 @@ export function WarningBox({ title, children }:{title:string|null, children:stri
              </p></div>
         </div>
         <div className=" m-[0.1rem]  hover:cursor-pointer" onClick={()=>{setUp(false)}} >
-          <XMarkIcon className=" h-5 w-5min-w-fit" />
+          <XMarkIcon className=" h-5 w-5 min-w-fit" />
         </div>
       </div>
       ) : null
