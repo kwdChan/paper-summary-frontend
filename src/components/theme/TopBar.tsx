496e906987ebd7394aa0438caeecf3e4c0a64ddb
dan <dan>
2023-06-08 20:45:32 +1000

    top bar refactor

---
+++ src/components/theme/TopBar.tsx
@@ -0,0 +1,16 @@
+export function TopBar({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center justify-between relative bg-blue-500 text-white h-14 w-screen overflow-ellipsis p-1">
+        {children}
+    </div>
+  );
+}
+
+TopBar.Title = function TopBarTitle({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="w-8/12 px-2 ">
+          <b>{children}</b>
+        </div>
+    );
+  }
+  
